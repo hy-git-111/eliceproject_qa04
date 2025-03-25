7b2f151cf7d2be099fa88d5a942fdbc00c7c89ac
Quokka_k <33806399+simple9518@users.noreply.github.com>
2025-03-25 22:47:47 +0900

    mypage profile update

--- src/pages/my_page.py
+++ src/pages/my_page.py
@@ -3,37 +3,34 @@ import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.webdriver import WebDriver
 
-from tests.conftest import driver
+
 
 class MyPage():
     def __init__(self, driver):
         self.driver = driver()
 
-    # 마이페이지 버튼 클릭
-    def open_mypage(self, driver):
+    # 개인 피드 버튼 클릭
+    def open_mypage(self,driver):
         mypage_btn = driver.find_element(By.XPATH, '//*[@id="root"]/div[1]/div/ul/li[4]/a')
         mypage_btn.click()
         time.sleep(2)
 
-    # 프로필 요소 확인
-    def my_profile(self, driver):
-        # 프로필 이미지
-        profile_img = driver.find_element(By.XPATH, '//*[@id="root"]/div[1]/main/section/section/section/div[1]/div[1]/img')
-        if profile_img:
-            print("프로필 이미지 확인")
-        else:
-            print("프로필 이미지 미확인")
+    # 프로필 수정하기 버튼 클릭
+    def profile_setup(self, driver):
+        porfile_setup_btn = driver.find_element(By.XPATH, '//*[@id="root"]/div[1]/main/section/section/section/div[1]/div[2]/div/svg/path')
+        porfile_setup_btn.click()
 
-        # 팀명
-        profile_team = driver.find_element(By.XPATH, '//*[@id="root"]/div[1]/main/section/section/section/div[1]/div[2]/div/div')
-        if profile_team:
-            print("팀명 확인")
-        else:
-            print("팀명 미확인")
+    # 프로필 정보 수정 - 이미지 첨부 버튼 클릭
+    # 여기서부터 다시 작업 예정
+    # def image_attach(self):
 
-        # 유저명
-        profile_name = driver.find_element(By.XPATH, '//*[@id="root"]/div[1]/main/section/section/section/div[1]/div[2]/span').text
-        if profile_name:
-            print(f"사용자명: {profile_name}")
-        else:
-            print("사용자명 미식별")
+    # 내가 먹은 메뉴
+    def my_food_review(self, driver):
+        my_food_add_btn = driver.find_element(By.XPATH, '//*[@id="root"]/div[1]/main/section/section/div[2]/div[1]/button')
+        my_food_add_btn.click()
+        time.sleep(2)
+
+    # 같은 메뉴 먹기
+    def same_food_review(self, driver):
+        same_food_review_btn = driver.find_element(By.XPATH, '//*[@id="root"]/div[1]/main/section/section/div[2]/div[2]/div[1]/div[2]/button')
+        same_food_review_btn.click()
---
+++ tests/test_my_page.py
@@ -0,0 +1,32 @@
+import time
+import pytest
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.webdriver import WebDriver
+
+class MyPage_Profile():
+    # 프로필 요소 확인
+    def my_profile(self, driver):
+        # 프로필 이미지
+        profile_img = driver.find_element(By.XPATH,
+                                          '//*[@id="root"]/div[1]/main/section/section/section/div[1]/div[1]/img')
+        if profile_img:
+            print("프로필 이미지 확인")
+        else:
+            print("프로필 이미지 미확인")
+
+        # 팀명
+        profile_team = driver.find_element(By.XPATH,
+                                           '//*[@id="root"]/div[1]/main/section/section/section/div[1]/div[2]/div/div')
+        if profile_team:
+            print("팀명 확인")
+        else:
+            print("팀명 미확인")
+
+        # 유저명
+        profile_name = driver.find_element(By.XPATH,
+                                           '//*[@id="root"]/div[1]/main/section/section/section/div[1]/div[2]/span').text
+        if profile_name:
+            print(f"사용자명: {profile_name}")
+        else:
+            print("사용자명 미식별")
