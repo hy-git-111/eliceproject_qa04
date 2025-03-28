f66af5ff18dcc7df5b1fc4b1fb2c3c95526c9387
Quokka_k <33806399+simple9518@users.noreply.github.com>
2025-03-28 20:55:07 +0900

    Update mypage

--- src/pages/my_page.py
+++ src/pages/my_page.py
@@ -14,6 +14,17 @@ class MyPage():
         mypage_btn.click()
         time.sleep(2)
 
+    # def profile_image(self):
+        # try:
+        #     img_src = profile_img.get_attribute("src")
+        #
+        #     # URL에서 마지막 '/' 이후의 파일명 추출
+        #     if img_src:
+        #         return img_src.split("/")[-1]
+        #     return None
+        # except:
+        #     return None
+
     # 프로필 수정하기 버튼 클릭
     def profile_setup(self):
         porfile_setup_btn = self.driver.find_element(By.XPATH, '//*[@id="root"]/div[1]/main/section/section/section/div[1]/div[2]/div/svg/path')
@@ -47,4 +58,18 @@ class MyPage():
         same_food_review_btn = self.driver.find_element(By.XPATH, '//*[@id="root"]/div[1]/main/section/section/div[2]/div[2]/div[1]/div[2]/button')
         same_food_review_btn.click()
 
+# 후기 목록 가져오기
+    # 하단 스크롤 > 요소 긁어오기 /> 로딩 > 다음 리스트 긁어오기 - 로딩까지 필요한가?
+        # user_data에 비교할 기존 리스트 추가하기
+        # page에 필요한 내용
+            # 스크롤, 동일 목록 클래스 텍스트 가져오기 (로딩 후 스크롤은 잠시 보류)
+        # test에 필요한 내용
+            # 가져온 텍스트와 기존 데이터 비교하기
 
+
+    # 페이지 스크롤
+    def scroll_to_top(self):
+        self.scroll_handler.scroll_to_top()
+
+    def scroll_to_bottom(self):
+        return self.scroll_handler.scroll_to_bottom()
