99da3b340a05542f5a617eb55a5baf87e63e73ef
Quokka_k <33806399+simple9518@users.noreply.github.com>
2025-03-31 21:08:29 +0900

    update my_page

--- src/pages/my_page.py
+++ src/pages/my_page.py
@@ -1,8 +1,12 @@
 import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
 from tests.conftest import driver
-from ..utils.helpers import WebUtils
+
+
 
 class MyPage():
     def __init__(self, driver:WebDriver):
@@ -12,7 +16,6 @@ class MyPage():
     def open_mypage(self):
         mypage_btn = self.driver.find_element(By.XPATH, '//*[@id="root"]/div[1]/div/ul/li[4]/a')
         mypage_btn.click()
-        time.sleep(2)
 
     # def profile_image(self):
         # try:
@@ -25,10 +28,11 @@ class MyPage():
         # except:
         #     return None
 
-    # 프로필 수정하기 버튼 클릭
+    # 프로필 수정하기 버튼 클릭- 너무 안눌림..
     def profile_setup(self):
-        porfile_setup_btn = self.driver.find_element(By.XPATH, '//*[@id="root"]/div[1]/main/section/section/section/div[1]/div[2]/div/svg/path')
-        porfile_setup_btn.click()
+        container = self.driver.find_element(By.CSS_SELECTOR, "div.container-class")
+        profile_setup_btn = container.find_elements(By.CSS_SELECTOR, "cursor-pointer")[1]
+        profile_setup_btn.click()
 
     # 프로필 정보 수정 - 이미지 첨부 버튼 클릭
     def image_attach(self):
@@ -49,14 +53,44 @@ class MyPage():
 
     # 내가 먹은 메뉴 추가하기 버튼 클릭
     def my_food_review(self):
-        my_food_add_btn = self.driver.find_element(By.XPATH, '//*[@id="root"]/div[1]/main/section/section/div[2]/div[1]/button')
+        my_food_add_btn = WebDriverWait(self.driver, 5).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="root"]/div[1]/main/section/section/div[2]/div[1]/button'))
+        )
+        # my_food_add_btn = self.driver.find_element(By.CSS_SELECTOR, "button.bg-main-black.text-white")
         my_food_add_btn.click()
-        time.sleep(2)
+
+        # 요소를 중앙에 위치하게 스크롤
+    def scroll_to_element(self, by, value):
+        element = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((by, value))
+        )
+        self.driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", element)
+        WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((by, value))).click()
+
 
     # 같은 메뉴 먹기 버튼 클릭
     def same_food_review(self):
-        same_food_review_btn = self.driver.find_element(By.XPATH, '//*[@id="root"]/div[1]/main/section/section/div[2]/div[2]/div[1]/div[2]/button')
+        same_food_review_btn = self.driver.find_element(By.XPATH, '//*[@id="root"]/div[1]/main/section/section/div[2]/div[1]/button')
         same_food_review_btn.click()
+
+    def get_menu_info(self):
+        recent_review = self.driver.find_elements(By.CSS_SELECTOR, ".flex.w-full.gap-6.p-4.shadow-md.rounded-2xl")
+        if not recent_review:
+            return {}
+        first_review = recent_review[0]
+
+        return {
+            "image_src": first_review.find_element(By.CSS_SELECTOR, "img.object-cover").get_attribute("src"),
+            "tags": [
+                elem.text for elem in first_review.find_elements(By.CSS_SELECTOR, ".inline-flex.items-center")
+            ],
+            "title": first_review.find_element(By.CSS_SELECTOR, ".font-bold").text,
+            "rating": "".join(
+                elem.text for elem in first_review.find_elements(By.CSS_SELECTOR, ".flex.gap-0.5 span")
+            ),
+            "review": first_review.find_element(By.CSS_SELECTOR, ".text-description").text,
+        }
+
 
 # 후기 목록 가져오기
     # 하단 스크롤 > 요소 긁어오기 /> 로딩 > 다음 리스트 긁어오기 - 로딩까지 필요한가?
@@ -67,9 +101,3 @@ class MyPage():
             # 가져온 텍스트와 기존 데이터 비교하기
 
 
-    # 페이지 스크롤
-    def scroll_to_top(self):
-        self.scroll_handler.scroll_to_top()
-
-    def scroll_to_bottom(self):
-        return self.scroll_handler.scroll_to_bottom()
